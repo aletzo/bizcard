d511d473f1f65cde66a6fbce1f3183453e352ab3
ale tzo <ajo@mydevnull.net>
2011-07-01 23:45:25 +0300

    started the project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>hi!</title>
+        <meta name="description" content="" />
+        <meta name="author" content="ajo" />
+        <script src="./js/libs/processingjs/processing-1.2.1.min.js"></script>
+    </head>
+    <body>
+        hi!
+        <script>
+            var _gaq=[["_setAccount","UA-6990380-2"],["_trackPageview"]];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+                g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+                s.parentNode.insertBefore(g,s)}(document,"script"));
+        </script>
+    </body>
+</html>
