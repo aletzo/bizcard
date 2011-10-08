993ffffdfdcbc062937b4f88a31c11c0b49a4cd1
ale tzo <ajo@mydevnull.net>
2011-10-09 00:00:17 +0300

    added a cooking page
    nothing fancy (yet)

---
+++ cooking/js/app.js
@@ -0,0 +1,12 @@
+$('#noodles_no').colorbox({
+    html: $('#noodles_no_content').html(),
+    innerHeight: 300,
+    innerWidth: 600,
+    opacity: 0.7
+});
+$('#noodles_yes').colorbox({
+    html: $('#noodles_yes_content').html(),
+    innerHeight: 300,
+    innerWidth: 850,
+    opacity: 0.7
+});
